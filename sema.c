6994c5296d3b73e11cbe3a0eede37e6325ea6198
toxic <130428178+Toxic-311@users.noreply.github.com>
2023-12-08 15:16:16 +0530

    sema.c

--- sema.c
+++ sema.c
@@ -1,4 +1,4 @@
-#include<stdio.> 
+#include<stdio.h> 
 void main()
 {
 int buffer[10], bufsize, in, out, produce, consume, 
@@ -7,27 +7,26 @@ out = 0;
 bufsize = 10;
 while(choice !=3)
 {
-printf(“\n1. Produce \t 2. Consume \t3. Exit”); 
-printf(“\nEnter your choice: ”);
-scanf(“%d”,&choice); 
+printf("\n1. Produce \t 2. Consume \t3. Exit"); 
+printf("\nEnter your choice: ");
+scanf("%d",&choice); 
 switch(choice) {
 case 1: if((in+1)%bufsize==out)
-printf(“\nBuffer is Full”);
+printf("\nBuffer is Full");
 else
 {
-}
-break;;;
-printf(“\nEnter the value: “); 
-scanf(“%d”, &produce); 
+printf("\nEnter the value: "); 
+scanf("%d", &produce); 
 buffer[in] = produce;
 in = (in+1)%bufsize;
+} break;
 case 2: if(in == out)
-printf(“\nBuffer is Empty”);
-} } }
+printf("\nBuffer is Empty");
 else
 {
 consume = buffer[out];
-printf(“\nThe consumed value is %d”, consume); 
+printf("\nThe consumed value is %d", consume); 
 out = (out+1)%bufsize;
 }
 break;
+}}}
