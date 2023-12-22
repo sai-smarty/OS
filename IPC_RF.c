493770ce837e6ac80dff67cd16311ec301127adc
toxic <130428178+Toxic-311@users.noreply.github.com>
2023-12-22 14:54:07 +0530

    Update IPC_RF.c

--- IPC_RF.c
+++ IPC_RF.c
@@ -6,22 +6,16 @@
 #include <unistd.h> 
 int main() {
 int fd1;
-// FIFO file path
+
 char * myfifo = "/tmp/myfifo";
-// Creating the named file(FIFO)
-// mkfifo(<pathname>,<permission>) 
 mkfifo(myfifo, 0666);
 char str1[80], str2[80]; 
 while (1) {
-// First open in read only and read 
 fd1 = open(myfifo,O_RDONLY); 
 read(fd1, str1, 80);
-// Print the read string and close 
 printf("User1: %s
 \n", str1); 
 close(fd1);
-// Now open in write mode and write
-// string taken from user.
 fd1 = open(myfifo,O_WRONLY); 
 fgets(str2, 80, stdin);
 write(fd1, str2, strlen(str2)+1); 
